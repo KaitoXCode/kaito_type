975e983c37b8467d08499313fdb72f77f6ff641d
KaitoXCode <gerhard.rahn1@gmail.com>
2023-12-18 18:49:46 +0000

    expanding errors

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -2,12 +2,22 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use serde::Serialize;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "tag", content = "data")]
 pub enum Error {
     LoginFail,
+
+    // -- Auth errors
+    AuthFailNoAuthTokenCookie,
+    AuthFailInvalidToken,
+    AuthFailCtxNotInRequestExt,
+
+    // -- Model errors
+    ScriptDeleteFailIdNotFound { id: u64 },
 }
 
 // region:      ---Error boilerplate
@@ -24,12 +34,44 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        // Create a placeholder Axum reponse.
+        // create a placeholder Axum reponse.
         let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
 
-        // Insert the Error into the reponse.
+        // insert the Error into the reponse.
         response.extensions_mut().insert(self);
 
         response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            // -- login fail
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            // -- auth
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailInvalidToken
+            | Self::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+            // -- model
+            Self::ScriptDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+            // -- fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
