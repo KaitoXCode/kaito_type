439f5ca54ba26dde32b6ece1199260cf20a7b5a6
KaitoXCode <gerhard.rahn1@gmail.com>
2023-12-18 18:52:27 +0000

    adding all previous changes to main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,26 +1,49 @@
 pub use self::error::{Error, Result};
+use crate::ctx::Ctx;
+use crate::log::log_request;
 use axum::{
     extract::{Path, Query},
-    response::{Html, IntoResponse},
+    http::{Method, Uri},
+    middleware,
+    response::{Html, IntoResponse, Response},
     routing::{get, get_service},
-    Router,
+    Json, Router,
 };
 use serde::Deserialize;
+use serde_json::json;
 use tokio::net::TcpListener;
+use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
+mod ctx;
 mod error;
+mod log;
+mod model;
 mod web;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     // watch application level logs: cargo watch -q -c -w src/ -x run
     // watch test level logs: cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
-    println!("Launching Backend...");
+    println!("->> Launching Backend...");
+    // initialise modelcontroller
+    let mc = model::ModelController::new().await?;
+
+    let routes_apis = web::routes_scripts::routes(mc.clone())
+        .route_layer(middleware::from_fn(web::mw_auth::mw_require_auth));
+
     // define routes:
     let routes_all = Router::new()
-        .merge(routes_all())
+        .merge(routes_hello())
         .merge(web::routes_login::routes())
+        .nest("/api", routes_apis)
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(middleware::from_fn_with_state(
+            mc.clone(),
+            web::mw_auth::mw_ctx_resolver,
+        ))
+        .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
     // region: --- Start Server
     let listner = TcpListener::bind("127.0.0.1:3000").await.unwrap();
@@ -28,6 +51,7 @@ async fn main() {
     axum::serve(listner, routes_all.into_make_service())
         .await
         .unwrap();
+    Ok(())
     // // build our application with a single route
     // let app = Router::new().route("/hello", get(|| async { Html("Hello <strong>World!!!</strong>") }));
     //
@@ -41,8 +65,40 @@ fn routes_static() -> Router {
     Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
 
+async fn main_response_mapper(
+    ctx: Option<Ctx>,
+    uri: Uri,
+    req_method: Method,
+    res: Response,
+) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+    // -- Get eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+    // -- if client error, build new response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+            println!("  ->> client_error_body: {client_error_body}");
+            // build the new response from the client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+    // build and log the server log line
+    let client_error = client_status_error.unzip().1;
+    let _ = log_request(uuid, req_method, uri, ctx, service_error, client_error).await;
+    println!();
+    error_response.unwrap_or(res)
+}
+
 // region:      --- Routes Hello
-fn routes_all() -> Router {
+fn routes_hello() -> Router {
     Router::new()
         .route("/hello", get(handler_hello))
         .route("/hello2/:name", get(handler_hello2))
