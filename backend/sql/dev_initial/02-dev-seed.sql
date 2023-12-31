52432e0d5c3ebfb73d769be34071982ce1426cc5
KaitoXCode <gerhard.rahn1@gmail.com>
2023-12-31 12:16:25 +0000

    add 5x example scripts for rand select

--- backend/sql/dev_initial/02-dev-seed.sql
+++ backend/sql/dev_initial/02-dev-seed.sql
@@ -1,2 +1,9 @@
 -- User demo1
 INSERT INTO "user" (username) VALUES ('demo1');
+
+-- Example scripts
+INSERT INTO "script" (text) VALUES ('This is an example script which was seeded on app init! #1');
+INSERT INTO "script" (text) VALUES ('This is an example script which was seeded on app init! #2');
+INSERT INTO "script" (text) VALUES ('This is an example script which was seeded on app init! #3');
+INSERT INTO "script" (text) VALUES ('This is an example script which was seeded on app init! #4');
+INSERT INTO "script" (text) VALUES ('This is an example script which was seeded on app init! #5');
